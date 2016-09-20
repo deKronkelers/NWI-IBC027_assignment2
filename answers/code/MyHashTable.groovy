f4cb0a79d9138c98dc08f46d26cf111358803629
Hendrikto <hendrik.to@gmail.com>
2016-09-20 20:05:56 +0200

    Add an answer to question 4.

---
+++ answers/code/MyHashTable.groovy
@@ -0,0 +1,44 @@
+class MyHashTable<T> {
+    private final ListNode<T>[] array
+
+    MyHashTable(int slots) {
+        array = (1..slots).collect { new ListNode<T>() }
+    }
+
+    void insert(T elem) {
+        ListNode<T> listNode = array[elem.hashCode() % array.length]
+        while (!listNode.value.is(null) && listNode.nextNode) {
+            listNode = listNode.nextNode
+        }
+        if (listNode.value.is(null)) {
+            listNode.value = elem
+        } else {
+            ListNode<T> newNode = new ListNode<>()
+            newNode.value = elem
+            listNode.nextNode = newNode
+        }
+    }
+
+    void delete(T elem) {
+        ListNode<T> listNode = array[elem.hashCode() % array.length]
+        while (listNode.value != elem && listNode.nextNode) {
+            listNode = listNode.nextNode
+        }
+        if (listNode.value == elem) {
+            listNode.value = null
+        }
+    }
+
+    boolean search(T elem) {
+        ListNode<T> listNode = array[elem.hashCode() % array.length]
+        while (listNode.value != elem && listNode.nextNode) {
+            listNode = listNode.nextNode
+        }
+        listNode.value == elem
+    }
+
+    private class ListNode<T> {
+        ListNode<T> nextNode
+        T value
+    }
+}
